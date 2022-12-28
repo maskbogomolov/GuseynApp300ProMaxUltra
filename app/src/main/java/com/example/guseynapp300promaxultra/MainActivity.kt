beae534c8e563462d4db931e6f10b9ed23f6ab97
Богомолов <bogomolov-mv@mosmetro.ru>
2022-12-28 18:23:55 +0300

    Добавил edge-to-edge и SlotFragment.kt

--- app/src/main/java/com/example/guseynapp300promaxultra/MainActivity.kt
+++ app/src/main/java/com/example/guseynapp300promaxultra/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.guseynapp300promaxultra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.guseynapp300promaxultra.utils.EdgeToEdgeDecorator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureEdgeToEdge()
+    }
+
+    private fun configureEdgeToEdge() {
+        EdgeToEdgeDecorator()
+            .updateConfig { isEdgeToEdgeEnabled = true }
+            .apply(this, window)
     }
 }
